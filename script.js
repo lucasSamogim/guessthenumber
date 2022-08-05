0d4b22961e61fb97d2d9496a6d33183be3ec469a
lucasSamogim <lsamogim.dev@gmail.com>
2022-08-04 23:22:23 -0300

    Changes in footer and comments in js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//Initializing the Variables
 let startArea = document.querySelector('.start-area');
 let winArea = document.querySelector('.win-area');
 let guessNumber = document.querySelector("#guess-number");
@@ -7,9 +8,9 @@ let btnGuess = document.querySelector('.btn-guess');
 let btnRestart = document.querySelector('.btn-restart');
 let randomNumber = Math.floor(Math.random() * 11);
 
-//Button Events
+//Button Events and Click Functions
 btnGuess.addEventListener('click', () => {
-    let number = Number(guessNumber.value);
+    let number = Number(guessNumber.value);     //Getting the value of the input every time we click the GUESS button
     if(number === randomNumber) {
             startArea.style.display = "none";
             winArea.style.display = "flex";
@@ -19,6 +20,6 @@ btnGuess.addEventListener('click', () => {
 btnRestart.addEventListener('click', () => {
     startArea.style.display = "flex";
     winArea.style.display = "none";
-    guessNumber.value = "";
-    randomNumber = Math.floor(Math.random() * 11);
+    guessNumber.value = "";                     //cleaning the input when we reset the game
+    randomNumber = Math.floor(Math.random() * 11);      //Restarting the random number for the next game
 })
