23645dee595c99ba6bf7ac3ebb9e4446e673b4c3
lucasSamogim <lsamogim.dev@gmail.com>
2022-08-08 11:17:52 -0300

    Add resources of error treatment to the logic

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 "use strict";
 
-//Initializing the Variables
+//Initializing the Variables - Getting elements from DOM
 let startArea = document.querySelector('.start-area');
 let winArea = document.querySelector('.win-area');
+let wrongMsg = document.querySelector('.wrong-msg');
+let errorMsg = document.querySelector('.error-msg');
 let guessNumber = document.querySelector("#guess-number");
 let btnGuess = document.querySelector('.btn-guess');
 let btnRestart = document.querySelector('.btn-restart');
+//Generating the first random number
 let randomNumber = Math.floor(Math.random() * 11);
 
 //Button Events and Click Functions
 btnGuess.addEventListener('click', () => {
     let number = Number(guessNumber.value);     //Getting the value of the input every time we click the GUESS button
-    if(number === randomNumber) {
+
+    if(number < 0 || number > 10) {             //Showing the error msg when entered the wrong interval
+        if(wrongMsg.style.display === "block") {    //If the msg of wrong number is displayed and the user type the wrong interval the wrongMsg leave the page
+            wrongMsg.style.display = "none";
+            errorMsg.style.display = "block"
+        } else {
+            errorMsg.style.display = "block";
+        }
+    } else {
+        if(number === randomNumber) {
+            errorMsg.style.display = "none";
             startArea.style.display = "none";
+            wrongMsg.style.display = "none";
             winArea.style.display = "flex";
+        } else {
+            errorMsg.style.display = "none";
+            wrongMsg.style.display = "block";
+        }
     }
 })
 
